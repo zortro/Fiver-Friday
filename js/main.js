18f0ba9316417e8a8721685e439ba01555a2c0a7
ZETA <ben.r.dominguez@gmail.com>
2023-02-02 11:14:16 -0800

    added playerModel and minor functionality

--- js/main.js
+++ js/main.js
@@ -11,20 +11,32 @@ function genCount() {
     }
 
     for (let i = 0; i < quantity.value; i++) {
-        const newLabel = document.createElement('label')
-        newLabel.setAttribute('for', 'Name')
-        newLabel.innerHTML = 'Player Name:'
+        const nameLabel = document.createElement('label')
+        nameLabel.setAttribute('for', 'Name')
+        nameLabel.innerHTML = `Player ${i + 1} Name:`
         
-        const newInput = document.createElement('input')
-        newInput.setAttribute('type', 'text')
-        newInput.setAttribute('id', `name${i+1}`)
-        newInput.setAttribute('name', 'name')
-        newInput.setAttribute('placeholder', 'FirstName')
+        const nameInput = document.createElement('input')
+        nameInput.setAttribute('type', 'text')
+        nameInput.setAttribute('id', `name${i+1}`)
+        nameInput.setAttribute('name', 'name')
+        nameInput.setAttribute('placeholder', 'FirstName')
 
+        //get labels for all bets to display on call
+        const chickenLabel = document.createElement('label')
+        chickenLabel.setAttribute('for', 'chickenWashington')
+        chickenLabel.innerHTML = 'Chicken Washington:'
+        
+        const chickenInput = document.createElement('input')
+        chickenInput.setAttribute('type', 'text')
+        chickenInput.setAttribute('id', `chickenWashington${i+1}`)
+        chickenInput.setAttribute('name', 'chickenWashington')
+        chickenInput.setAttribute('placeholder', '1, 2 ,3...')
 
-        newLabel.appendChild(newInput)
-        newInput.appendChild(newBr)
-        document.getElementById('nameForm').prepend(newLabel)
+        nameLabel.appendChild(nameInput)
+        nameInput.appendChild(newBr)
+        chickenLabel.appendChild(newBr)
+        chickenInput.appendChild(chickenLabel)
+        document.getElementById('nameForm').prepend(nameLabel)
     }
 
     document.getElementById('countGen').classList.add('hide')
@@ -33,11 +45,25 @@ function genCount() {
 
 let playerArr = []
 
+function playerObj(name, cwBet, obBet, pcBet, tlBet, prBet) {
+    this.name = name,
+    this.cwBet =cwBet,
+    this.obBet = obBet,
+    this.pcBet = pcBet,
+    this.tlBet = tlBet,
+    this.prBet = prBet
+}
+
 function genPlayers() {
     
     for (let i = 0; i < quantity.value; i++) {
         
         const names = document.getElementById(`name${i+1}`).value
+/*         const cwBet = document.getElementById(`name${i+1}`).value
+        const obBet = document.getElementById(`name${i+1}`).value
+        const pcBet = document.getElementById(`name${i+1}`).value
+        const tlBet = document.getElementById(`name${i+1}`).value
+        const prBet = document.getElementById(`name${i+1}`).value */
 
         if (!names) {
             console.log('name required in field')
@@ -45,6 +71,8 @@ function genPlayers() {
         }
 
         //create or init new player obj
+        const player = new playerObj(`${names}`, 1, 2, 3, 4, 5)
+        
 
         playerArr.push(player)
     }
@@ -52,7 +80,7 @@ function genPlayers() {
     nameGen.classList.add('hide')
     document.getElementById('betGen').classList.remove('hide')
 
-    playerArr.forEach(player => {
+/*     playerArr.forEach(player => {
         const nameTag = document.createElement('p')
         nameTag.innerHTML = player.name
 
@@ -71,7 +99,7 @@ function genPlayers() {
 
         document.getElementById('betForm').prepend(nameTag)
         
-    })
+    }) */
 }
 
 function genBet() {
